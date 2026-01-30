15cc8e34e86c21ce9d3e51df4eeda715d191ad66
anikethibare264 <anikethibare264@gmail.com>
2026-01-30 15:17:26 +0530

    first commit

---
+++ prac.py
@@ -0,0 +1,28 @@
+# # # num1=int(input('enter the number: '))
+# # # num2=int(input('enter the number: '))
+
+# # # sum=num1 + num2 ,num1 - num2, num1 * num2 , num1 / num2
+
+# # # print(sum)
+
+
+
+# # for i in range (1,11):
+# #     # print(i*5)
+# #     print(f"2 * {i} = {2*i}")
+
+
+# for i in range(1,11):
+#     print(f"2 * {i} = {2*i}")
+
+# print("----------------------")
+
+# n=4
+# for i in range(n):
+#     print("*"*i)
+#     for j in range(n):
+#         print("*"*j)
+#     print("________________________")
+    
+for i in range(10,1,-1):
+    print("*" * i)
